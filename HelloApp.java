19c239b64f1b4c28fdd466e5b9719ffce01445ec
sparshchandra25 <sparshcollege25@gmail.com>
2026-03-17 14:02:37 +0530

    UC1done

--- HelloApp.java
+++ HelloApp.java
@@ -4,10 +4,11 @@ class HelloApp
     public static void main (String[] args)
 
     {
-        Scanner in=new Scanner (System.in);
+        Scanner in =new Scanner (System.in);
         System.out.println("What is your name");
         String s = in.nextLine();
         System.out.println("Hello "+ s);
         System.out.println("Welcome to the Hello App created by our developer");
+        in.close();
     }
 }

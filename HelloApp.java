269e350bf1c95c237209f958cd855a671c0608f1
Sparsh Chandra <sparshcollege25@gmail.com>
2026-03-26 15:15:39 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,14 +1,13 @@
-import java.util.*;
-class HelloApp
-{
-    public static void main (String[] args)
+public class HelloApp {
+    public static void main(String[] args) {
 
-    {
-        Scanner in =new Scanner (System.in);
-        System.out.println("What is your name");
-        String s = in.nextLine();
-        System.out.println("Hello "+ s);
-        System.out.println("Welcome to the Hello App created by our developer");
-        in.close();
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
     }
 }
